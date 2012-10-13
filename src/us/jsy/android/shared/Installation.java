70c7037cddf9a872929a944475e7bc40153e74b4
Sheng Yu <sheng@jsy.us>
2012-10-13 16:42:49 -0400

    start

---
+++ src/us/jsy/android/shared/Installation.java
@@ -0,0 +1,19 @@
+package us.jsy.android.shared;
+
+import java.util.UUID;
+
+import android.content.Context;
+
+public class Installation {
+	private static final String KEY = Installation.class.getName();
+
+	public static synchronized String getId(Context ctx) {
+		String id = Settings.get(ctx, KEY, null);
+		if (id == null) {
+			id = UUID.randomUUID().toString();
+			Settings.set(ctx, KEY, id);
+		}
+		
+		return id;
+	}
+}
