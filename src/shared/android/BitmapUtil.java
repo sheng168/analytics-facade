75798d8b9276a6e89baa9ca4ef17fdf7cfc3ba42
Sheng Yu <sheng@jsy.us>
2013-02-03 09:01:15 -0500

    add BitmapUtil and CheckableLinearLayout

---
+++ src/shared/android/BitmapUtil.java
@@ -0,0 +1,53 @@
+package shared.android;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class BitmapUtil {
+	static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(BitmapUtil.class);
+
+	public static Bitmap constraintBitmap(byte[] data, int reqWidth,
+			int reqHeight) {
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+
+		BitmapFactory.decodeByteArray(data, 0, data.length, options);
+		int imageHeight = options.outHeight;
+		int imageWidth = options.outWidth;
+//		String imageType = options.outMimeType;
+		log.debug("input image: {}x{}", imageWidth, imageHeight);
+
+		// Calculate inSampleSize
+		options.inSampleSize = calculateInSampleSize(options, reqWidth,
+				reqHeight);
+
+		// Decode bitmap with inSampleSize set
+		options.inJustDecodeBounds = false;
+		return BitmapFactory.decodeByteArray(data, 0, data.length, options);
+	}
+
+	public static int calculateInSampleSize(BitmapFactory.Options options,
+			int reqWidth, int reqHeight) {
+		// Raw height and width of image
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		int inSampleSize = 1;
+
+		if (height > reqHeight || width > reqWidth) {
+
+			// Calculate ratios of height and width to requested height and
+			// width
+			final int heightRatio = Math.round((float) height
+					/ (float) reqHeight);
+			final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+			// Choose the smallest ratio as inSampleSize value, this will
+			// guarantee
+			// a final image with both dimensions larger than or equal to the
+			// requested height and width.
+			inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+		}
+
+		return inSampleSize;
+	}
+}
