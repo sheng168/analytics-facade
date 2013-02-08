6eaed8dd3a4c24c422a2bbb8f64007b89498a351
Sheng Yu <sheng@jsy.us>
2013-02-08 14:33:14 -0500

    update to support any Checkable instead of just CheckedTextView

--- src/shared/android/CheckableLinearLayout.java
+++ src/shared/android/CheckableLinearLayout.java
@@ -4,14 +4,13 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Checkable;
-import android.widget.CheckedTextView;
 import android.widget.LinearLayout;
 
 /*
  * This class is useful for using inside of ListView that needs to have checkable items.
  */
 public class CheckableLinearLayout extends LinearLayout implements Checkable {
-	private CheckedTextView _checkbox;
+	private Checkable _checkbox;
     	
     public CheckableLinearLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -24,8 +23,8 @@ public class CheckableLinearLayout extends LinearLayout implements Checkable {
 		int childCount = getChildCount();
 		for (int i = 0; i < childCount; ++i) {
 			View v = getChildAt(i);
-			if (v instanceof CheckedTextView) {
-				_checkbox = (CheckedTextView)v;
+			if (v instanceof Checkable) {
+				_checkbox = (Checkable)v;
 			}
 		}    	
     }
